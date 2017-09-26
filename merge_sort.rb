1e48538d9669449cf79b084209b54f389ccaf7b8
Jacob C Moore <jmooree30@gmail.com>
2017-09-26 18:58:30 -0400

    Update merge_sort.rb

--- merge_sort.rb
+++ merge_sort.rb
@@ -4,12 +4,13 @@ def merge_sort(a)
   if a.length < 2
     return a 
   end 
-    sorted = []
-    arr1 = merge_sort(a[0..a.length/2-1])
-    arr2 = merge_sort(a[a.length/2..a.length])
-      until arr1.empty? or arr2.empty?
-        sorted << (arr1[0] > arr2[0] ? arr2.shift : arr1.shift)
-      end 
-      sorted.concat(arr1).concat(arr2)
+  sorted = []
+  arr1 = merge_sort(a[0..a.length/2-1])
+  arr2 = merge_sort(a[a.length/2..a.length])
+  until arr1.empty? or arr2.empty?
+    sorted << (arr1[0] > arr2[0] ? arr2.shift : arr1.shift)
+  end 
+  sorted.concat(arr1).concat(arr2)
 end
+
 merge_sort(a)
